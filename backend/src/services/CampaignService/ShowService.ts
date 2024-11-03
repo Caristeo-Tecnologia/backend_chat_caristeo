26978c52d24e62c72f2078cc547e38cc19edf6a6
Evandro Terra Gonçalves <960738@lojasrenner.com.br>
2024-11-03 12:47:55 -0400

    fix(campaigns): optional return contacts

--- backend/src/services/CampaignService/ShowService.ts
+++ backend/src/services/CampaignService/ShowService.ts
@@ -4,14 +4,27 @@ import CampaignShipping from "../../models/CampaignShipping";
 import ContactList from "../../models/ContactList";
 import ContactListItem from "../../models/ContactListItem";
 import Whatsapp from "../../models/Whatsapp";
+import { IncludeOptions } from "sequelize";
 
-const ShowService = async (id: string | number): Promise<Campaign> => {
+const ShowService = async (id: string | number, withContacts = false): Promise<Campaign> => {
+  const includeContactList = withContacts ? [
+    {
+      model: ContactListItem,
+      attributes: ["id", "name", "number", "email", "isWhatsappValid"]
+    }
+  ] : [];
+
+  const include: IncludeOptions[] = [
+    { model: CampaignShipping },
+    { model: Whatsapp, attributes: ["id", "name"] },
+    {
+      model: ContactList,
+      include: includeContactList
+    }
+  ]
+
   const record = await Campaign.findByPk(id, {
-    include: [
-      { model: CampaignShipping },
-      { model: ContactList, include: [{ model: ContactListItem }] },
-      { model: Whatsapp, attributes: ["id", "name"] }
-    ]
+    include
   });
 
   if (!record) {
